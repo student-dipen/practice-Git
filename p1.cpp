462430489d01704b862c9e0a0e8c7ea4337727fd
Student Dipen <baniyadipen9856@gmail.com>
2024-06-12 07:40:13 -1200

    first commit

---
+++ p1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+
+    cout<<"Enter number:"<<endl;
+    cin>>a;
+    cout<<"The entered numvber is:"<<a<<endl;
+}
---
+++ p2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter number:\n");
+    scanf("%d",a);
+
+    printf("the entered numbere is:",a);
+    return 0;
+}
